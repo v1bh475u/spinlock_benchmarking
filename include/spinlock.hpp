070126ea1a4a7c03cf03e81df03c03db4a2021f0
vibhatsu <maulikbarot2915@gmail.com>
2025-08-21 03:08:33 +0530

    feat: add double_check_lock2 class with improved spinning and CPU relaxation
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

--- include/spinlock.hpp
+++ include/spinlock.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <atomic>
 #include <memory>
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__)
+#include <immintrin.h>
+#endif
 
 namespace concurrency
 {
@@ -64,6 +67,50 @@ namespace concurrency
                     return;
             }
         }
+        void unlock()
+        {
+            lock_.store(false);
+        }
+    };
+    static inline void cpu_relax() noexcept
+    {
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__)
+        _mm_pause();
+#elif defined(__aarch64__) || defined(__arm__)
+        asm volatile("yield");
+#else
+        std::this_thread::yield();
+#endif
+    }
+
+    class double_check_lock2
+    {
+    private:
+        std::atomic<bool> lock_{false};
+
+    public:
+        void lock()
+        {
+            uint32_t spins = 1;
+            while (true)
+            {
+                while (lock_.load())
+                {
+                    cpu_relax();
+                }
+                bool expected = false;
+                if (lock_.compare_exchange_weak(expected, true))
+                {
+                    return;
+                }
+
+                for (uint32_t i = 0; i < spins; ++i)
+                    cpu_relax();
+                if (spins < (1u << 15))
+                    spins <<= 1;
+            }
+        }
+
         void unlock()
         {
             lock_.store(false);
