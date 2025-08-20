4c07ba7c6260f720dc3b1bc85589891dd177e4ab
vibhatsu <maulikbarot2915@gmail.com>
2025-08-21 02:18:08 +0530

    feat: implement base_lock and ticket_lock classes
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

---
+++ include/spinlock.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <atomic>
+#include <memory>
+
+namespace concurrency
+{
+    class base_lock
+    {
+    private:
+        std::atomic<bool> locked_{false};
+
+    public:
+        void lock()
+        {
+            bool expected = false;
+            while (!locked_.compare_exchange_weak(expected, true))
+            {
+                expected = false;
+            }
+        }
+        void unlock()
+        {
+            locked_.store(false);
+        }
+    };
+
+    class ticket_lock
+    {
+    private:
+        std::atomic<size_t> front_{0u};
+        std::atomic<size_t> back_{0u};
+
+    public:
+        void lock()
+        {
+            size_t ticket = back_.fetch_add(1);
+            while (true)
+            {
+                size_t current = front_.load();
+                if (current == ticket)
+                    return;
+            }
+        }
+        void unlock()
+        {
+            front_.fetch_add(1);
+        }
+    };
+}
